f42c28d1800dd27ba6a93c7533f519358df4effe
Enrique <earrieta.dev>
2021-08-01 21:57:32 -0400

    add new manage offer, buy offer and create pasive offer

--- src/app/state/wallets-operation.model.ts
+++ src/app/state/wallets-operation.model.ts
@@ -1,6 +1,26 @@
-import { ServerApi } from 'stellar-sdk';
+import { AssetType, ServerApi } from 'stellar-sdk';
 import { IWalletsAccount } from '~root/state/wallets-account.model';
 import OperationRecord = ServerApi.OperationRecord;
+
+interface ManageBuyOfferOperationResponse {
+  id: string;
+  paging_token: string;
+  source_account: string;
+  type: 'manage_buy_offer';
+  type_i: 12;
+  created_at: string;
+  transaction_hash: string;
+  offer_id: number | string;
+  amount: string;
+  buying_asset_type: AssetType;
+  buying_asset_code?: string;
+  buying_asset_issuer?: string;
+  price: string;
+  // price_r: PriceR;
+  selling_asset_type: AssetType;
+  selling_asset_code?: string;
+  selling_asset_issuer?: string;
+}
 
 export interface IWalletsOperation {
   _id: string;
@@ -8,7 +28,7 @@ export interface IWalletsOperation {
   ownerPublicKey: IWalletsAccount['publicKey'];
   createdAt: number; // Unix time
   pagingToken: OperationRecord['paging_token'];
-  operationRecord: OperationRecord;
+  operationRecord: OperationRecord | ManageBuyOfferOperationResponse;
 }
 
 export function createWalletsOperation(params: {
