8cb32eee94678b28dba60ee61897c24b8375c529
Enrique <earrieta.dev>
2021-08-11 13:49:01 -0400

    Allow checking in the blockchain when we are using the main network

--- src/app/modules/wallet/components/transaction-details/transaction-details.component.ts
+++ src/app/modules/wallet/components/transaction-details/transaction-details.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { IWalletsOperation } from '~root/state';
+import { HorizonApisQuery, IWalletsOperation } from '~root/state';
 import { ReplaySubject } from 'rxjs';
 import { filter, map, pluck, take } from 'rxjs/operators';
 import { GlobalsService } from '~root/lib/globals/globals.service';
 import { StellarSdkService } from '~root/gateways/stellar/stellar-sdk.service';
+import { Networks } from 'stellar-sdk';
 
 @Component({
   selector: 'app-transaction-details',
@@ -18,16 +19,24 @@ export class TransactionDetailsComponent implements OnInit {
 
   constructor(
     private readonly globalsService: GlobalsService,
+    private readonly horizonApisQuery: HorizonApisQuery,
   ) { }
 
   ngOnInit(): void {
   }
 
   async checkOnBlockchain(): Promise<void> {
+    const selectedHorizonApi = await this.horizonApisQuery.getSelectedHorizonApi$.pipe(take(1))
+      .toPromise();
+
+    const network = selectedHorizonApi.networkPassphrase === Networks.PUBLIC
+      ? 'public'
+      : 'testnet';
+
     const operation = await this.operation$.pipe(take(1)).toPromise();
     // TODO: This needs to be dynamic
     this.globalsService.window.open(
-      `https://stellar.expert/explorer/testnet/tx/${operation.operationRecord.transaction_hash}`,
+      `https://stellar.expert/explorer/${network}/tx/${operation.operationRecord.transaction_hash}`,
       '_blank'
     );
   }
