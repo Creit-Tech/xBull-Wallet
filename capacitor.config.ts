30971cea2eaa00281741d78fbf68a973dd05017f
Enrique <info@earrieta.dev>
2021-10-12 23:14:11 -0300

    Basic android configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.xbull.mobile',
+  appName: 'xBull-Wallet',
+  webDir: 'dist/xbull',
+  bundledWebRuntime: false
+};
+
+export default config;
