09f706283f0ba39557206f6b9e690cc5b0b59584
Enrique A <info@earrieta.dev>
2022-03-08 19:42:11 -0300

    Add custom horizon as default when using ios
    This is because Apple reviewers don't like that we use an endpoint that is not in our domain

--- src/environments/environment.mobile.prod.ts
+++ src/environments/environment.mobile.prod.ts
@@ -6,6 +6,12 @@ export const environment = {
   production: true,
   platform: 'mobile',
   defaultApis: [{
+    _id: 'aa604e66a74ade3ef250f904ef28c92f',
+    name: 'Creit Tech Horizon',
+    url: 'https://horizon.creit.tech',
+    networkPassphrase: Networks.PUBLIC,
+    canRemove: false,
+  }, {
     _id: 'aa604e66a74ade3ef250f904ef28c92d',
     name: 'Main Network',
     url: 'https://horizon.stellar.org',
