bc40d6c35822d03d42867cb12d34058b8745dbde
Enrique A <info@earrieta.dev>
2022-04-13 22:25:00 -0400

    old wallet module components removed

--- src/app/modules/wallet/wallet.module.ts
+++ src/app/modules/wallet/wallet.module.ts
@@ -2,24 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WalletRoutingModule } from './wallet-routing.module';
-import { WalletAssetsComponent } from './pages/wallet-assets/wallet-assets.component';
 import { SegmentModule } from '~root/shared/segment/segment.module';
-import { AssetItemComponent } from './components/asset-item/asset-item.component';
 import { ModalsModule } from '~root/shared/modals/modals.module';
-import { AddAssetComponent } from './components/add-asset/add-asset.component';
 import { FormsComponentsModule } from '~root/shared/forms-components/forms-components.module';
-import { SendFundsComponent } from './components/send-funds/send-funds.component';
-import { ReceiveFundsComponent } from './components/receive-funds/receive-funds.component';
 import { AssetDetailsComponent } from './components/asset-details/asset-details.component';
-import { WalletOffersComponent } from './pages/wallet-offers/wallet-offers.component';
-import { WalletComponent } from './wallet.component';
-import { OfferDetailsComponent } from './components/offer-details/offer-details.component';
-import { WalletTransactionsComponent } from './pages/wallet-transactions/wallet-transactions.component';
-import { TransactionDetailsComponent } from './components/transaction-details/transaction-details.component';
 import { LoadingModule } from '~root/shared/loading/loading.module';
 import { ClipboardModule } from '~root/shared/clipboard/clipboard.module';
 import { NgxMaskModule } from 'ngx-mask';
-import { WalletTransactionItemComponent } from './components/wallet-transaction-item/wallet-transaction-item.component';
 import { SharedPipesModule } from '~root/shared/shared-pipes/shared-pipes.module';
 import { NzImageModule } from 'ng-zorro-antd/image';
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -48,18 +37,7 @@ import { NzEmptyModule } from 'ng-zorro-antd/empty';
 
 @NgModule({
   declarations: [
-    WalletAssetsComponent,
-    AssetItemComponent,
-    AddAssetComponent,
-    SendFundsComponent,
-    ReceiveFundsComponent,
     AssetDetailsComponent,
-    WalletOffersComponent,
-    WalletComponent,
-    OfferDetailsComponent,
-    WalletTransactionsComponent,
-    TransactionDetailsComponent,
-    WalletTransactionItemComponent,
     WalletDashboardComponent,
     WalletAssetItemComponent,
     SendPaymentComponent,
