9f524f47b99f7720d4519e619d4aa875c494859b
Enrique A <info@earrieta.dev>
2022-08-13 23:56:59 -0400

    Allow merging accounts from the lab module

--- src/app/modules/lab/lab-routing.module.ts
+++ src/app/modules/lab/lab-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LabComponent } from '~root/modules/lab/lab.component';
 import { ImportXdrComponent } from '~root/modules/lab/pages/import-xdr/import-xdr.component';
+import { MergeAccountsComponent } from '~root/modules/lab/pages/merge-accounts/merge-accounts.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: 'import-xdr',
     component: ImportXdrComponent,
   },
+  {
+    path: 'merge-account',
+    component: MergeAccountsComponent,
+  }
 ];
 
 @NgModule({
